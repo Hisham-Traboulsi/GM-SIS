3811a3e48debbf8c8ecf2643eae7e2721c3d0616
Hisham Traboulsi <h.traboulsi@se15.qmul.ac.uk>
2017-01-18 00:39:23 +0000

    Adding common package Adding customers package

---
+++ GMSIS/src/customers/ComponentFolder.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package customers;
+
+/**
+ *
+ * @author hisha
+ */
+
+import javax.swing.*;
+
+public class ComponentFolder 
+{
+    public static void main (String [] args)
+    {
+        JOptionPane.showMessageDialog(null, "It works!");
+    }
+}
