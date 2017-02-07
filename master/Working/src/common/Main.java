d92cfd9af78205c4f510664072aaffa749c68b39
Hisham Traboulsi <h.traboulsi@se15.qmul.ac.uk>
2017-02-07 19:33:37 +0000

    Testing

--- master/Working/src/common/Main.java
+++ master/Working/src/common/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/common/gui/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/common/gui/Admin.fxml"));
         
         Scene scene = new Scene(root);
         
