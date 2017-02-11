691d88ef6bafc5babe9c85951f981591f7702fd2
DESKTOP-8PD0FFE\sergi <s.arrietasiyom@se15.qmul.ac.uk>
2017-02-11 20:23:50 +0000

    Changes to partController.java

--- master/Working/src/parts/gui/PartsController.java
+++ master/Working/src/parts/gui/PartsController.java
@@ -5,9 +5,19 @@
  */
 package parts.gui;
 
+import common.database.Database;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -15,13 +25,71 @@ import javafx.fxml.Initializable;
  * @author sergi
  */
 public class PartsController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    
+    ObservableList data = FXCollections.observableArrayList();
+    
+    @FXML
+    private TextField partID;
+    @FXML
+    private TextField partName;
+    @FXML
+    private TextArea partDesc;
+    @FXML
+    private TextField partAmount;
+    @FXML
+    private TextField partCost;
+    @FXML
+    private Button search;
+     @FXML
+    private Button update;
+     @FXML
+    private Button delete;
+     @FXML
+    private Button add;
+    @FXML
+    private TableView table;
+    @FXML
+    private TableColumn colID;
+    @FXML
+    private TableColumn colName;
+    @FXML
+    private TableColumn colDesc;
+    @FXML
+    private TableColumn colAmount;
+    @FXML
+    private TableColumn colCost;
+            
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        
+     colID.setCellValueFactory(new PropertyValueFactory<>("ID"));
+     colName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+     colDesc.setCellValueFactory(new PropertyValueFactory<>("Desc"));
+     colAmount.setCellValueFactory(new PropertyValueFactory<>("Amount"));
+     colCost.setCellValueFactory(new PropertyValueFactory<>("Cost"));
+        
+        
+    } 
     
+    @FXML
+   public boolean add()
+    {
+      
+      boolean allow_access=false;
+      
+      int id = Integer.parseInt(partID.getText());
+      String name = (partName.getText());
+      String desc = (partDesc.getText());
+      int amount = Integer.parseInt(partAmount.getText());
+      int cost = Integer.parseInt(partCost.getText());
+  
+      
+      allow_access = Database.getInstance().addPart(id,name,desc,amount,cost);
+        
+      return allow_access;
+    }
+        
 }
+
+    
+
