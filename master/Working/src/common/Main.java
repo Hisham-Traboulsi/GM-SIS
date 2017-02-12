5584615fa5d939e3a9e97925ded996f353cc8f50
hisha <hisha@DESKTOP-55E1P99>
2017-02-12 02:24:42 +0000

    Fixed up the main,

--- master/Working/src/common/Main.java
+++ master/Working/src/common/Main.java
@@ -12,25 +12,46 @@ package common;
  * @author hisha
  */
 
+import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
 public class Main extends Application
 {
+    
+    private static BorderPane root = new BorderPane();
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/parts/gui/parts.fxml"));
+        /*Parent root = FXMLLoader.load(getClass().getResource("/parts/gui/parts.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
         stage.show();
-        stage.setResizable(false);
+        stage.setResizable(false);*/
+        
+        URL loginUrl = getClass().getResource("/common/gui/Login.fxml");
+        AnchorPane loginPane = FXMLLoader.load(loginUrl);
+        
+        root.setCenter(loginPane);
+        
+        Scene scene = new Scene(root);
+        
+        scene
+              .getStylesheets()
+              .add(getClass()
+              .getResource("/common/gui/common_style.css")
+              .toExternalForm());
+        
+        stage.setScene(scene);
+        stage.show(); 
     }
 
     /**
@@ -38,6 +59,11 @@ public class Main extends Application
      */
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    public static BorderPane getRoot()
+    {
+        return root;
     }
     
 }
