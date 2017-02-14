5eedcded7072834c581dcb43ce428483bd55cbcd
Hisham Traboulsi <h.traboulsi@se15.qmul.ac.uk>
2017-02-14 20:15:22 +0000

    Revert "."
    This reverts commit 2eabd1423da52c95cb18c44b6670c6ae9aa9beb6.

--- master/Working/src/common/Main.java
+++ master/Working/src/common/Main.java
@@ -37,7 +37,7 @@ public class Main extends Application
         stage.show();
         stage.setResizable(false);*/
         
-        URL loginUrl = getClass().getResource("/common/gui/Login.fxml");
+        URL loginUrl = getClass().getResource("/parts/gui/parts.fxml");
         AnchorPane loginPane = FXMLLoader.load(loginUrl);
         
         root.setCenter(loginPane);
