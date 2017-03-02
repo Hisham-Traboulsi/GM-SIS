39c1b6cb692bbae2d4b93118e02bd8122bab1462
DESKTOP-2TNS4D9\ugonw <s.u.nwangwu@se15.qmul.ac.uk>
2017-03-02 09:50:18 +0000

    Vehicle class created

---
+++ master/Working/src/vehicles/logic/Vehicle.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicles.logic;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author ugonw
+ */
+public class Vehicle {
+    
+    private IntegerProperty id;
+    private StringProperty vehicletype;
+    private StringProperty regnum;
+    private StringProperty model;
+    private StringProperty make;
+    private StringProperty engine;
+    private StringProperty fueltype;
+    private StringProperty colour;
+    private StringProperty motdate;
+    private StringProperty lastservice;
+    private StringProperty mileage;
+    private StringProperty warranty;
+    private StringProperty warrantycompany;
+    private StringProperty warrantyaddress;
+    private StringProperty warrantyexpiry;
+            
+    
+   
+    public Vehicle(String regnum, String model , String make, String engine, String fueltype, String colour, String motdate, String lastservice, String mileage, String warranty, String warrantycompany, String warrantyaddress, String warrantyexpiry)
+    {
+        
+        this.regnum= new SimpleStringProperty(regnum);
+        this.model=  new SimpleStringProperty(model);
+        this.make = new SimpleStringProperty(engine);
+        this.engine = new SimpleStringProperty(engine);
+        this.fueltype = new SimpleStringProperty(fueltype);
+        this.colour = new SimpleStringProperty(colour);
+        this.motdate = new SimpleStringProperty(motdate);
+        this.lastservice = new SimpleStringProperty(lastservice);
+        this.mileage = new SimpleStringProperty(mileage);
+        this.warranty = new SimpleStringProperty(warranty);
+        this.warrantycompany = new SimpleStringProperty(warrantycompany);
+        this.warrantyaddress = new SimpleStringProperty(warrantyaddress);
+        this.warrantyexpiry = new SimpleStringProperty(warrantyexpiry);
+    }
+    
+    public int getID()
+    {
+        return id.get();
+    }
+    
+    public String getVehicleType()
+    {
+        return vehicletype.get();
+    }
+    
+    public String regnum()
+    {
+        return regnum.get();
+    }
+    
+     public String getModel()
+    {
+        return model.get();
+    
+    }
+    
+    public String getMake()
+    {
+        return make.get();
+    }
+    
+    public String getEngine()
+    {
+        return engine.get();
+    }
+    
+    public String getFuelType()
+    {
+        return fueltype.get();
+    }
+    
+    public String getColour(){
+        
+        return colour.get();
+    }
+    
+    public String getMotDate()
+    {
+        return motdate.get();
+    }
+    
+    public String lastService()
+    {
+        return lastservice.get();
+    }
+    
+    public String getMileage()
+    {
+        return mileage.get();
+    }
+    
+    public String getWarranty()
+    {
+        return warranty.get();
+    }
+    
+    public void setID(int id)
+    {
+        this.id.set(id);
+    }
+    
+    public void setVehicleType(String vehicletype)
+    {
+        this.vehicletype.set(vehicletype);
+    }
+    
+    public void setRegNum(String regnum)
+    {
+        this.regnum.set(regnum);
+    }
+    
+    public void setModel(String model)
+    {
+        this.model.set(model);
+    }
+    
+    public void setMake(String make)
+    {
+        this.make.set(make);        
+    }
+    
+    public void setEngine(String engine)
+    {
+        this.engine.set(engine);
+    }
+    
+    public void setFuelType(String fueltype)
+    {
+        this.fueltype.set(fueltype);
+    }
+            
+    public void setColour(String colour)
+    {
+        this.colour.set(colour);
+    }
+    
+    public void setMotDate(String motdate)
+    {
+        this.motdate.set(motdate);
+    }
+    
+    public void lastService(String lastservice)
+    {
+        this.lastservice.set(lastservice);
+    }
+   
+    public void setMileage(String mileage)
+    {
+        this.mileage.set(mileage);
+    }
+
+    public void setWarranty(String warranty)
+    {
+        this.warranty.set(warranty);
+    }
+    
+}
