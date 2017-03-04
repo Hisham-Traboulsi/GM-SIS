432b9eee7331800f5c94003e29e0b6e571047a36
DESKTOP-2TNS4D9\ugonw <s.u.nwangwu@se15.qmul.ac.uk>
2017-03-04 16:56:13 +0000

    Add Vehicle form values now add to table/database + editable tableview

--- master/Working/src/vehicles/logic/addVehicle.java
+++ master/Working/src/vehicles/logic/addVehicle.java
@@ -6,10 +6,14 @@
 package vehicles.logic;
 
 import common.Main;
+import common.database.Database;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -17,11 +21,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.util.converter.IntegerStringConverter;
+import parts.logic.Part;
 
 /**
  * FXML Controller class
@@ -84,18 +93,21 @@ public class addVehicle implements Initializable {
         expirydate_text.setVisible(false);
         expirydate.setVisible(false);
     }
-    
+
     @FXML
     private void goBack(ActionEvent e) {
-        try {
-
-            URL addInstalledPartUrl = getClass().getResource("/common/gui/Welcome.fxml");
-            AnchorPane addInstalledPartPane = FXMLLoader.load(addInstalledPartUrl);
-
+        try
+        {
+            
+            URL addPartUrl = getClass().getResource("/vehicles/gui/VehicleHomepage.fxml");
+            AnchorPane addPartPane = FXMLLoader.load(addPartUrl);
+            
             BorderPane border = Main.getRoot();
-
-            border.setCenter(addInstalledPartPane);
-        } catch (IOException ex) {
+            
+            border.setCenter(addPartPane);
+        }
+        catch(IOException ex)
+        {
             ex.printStackTrace();
         }
     }
@@ -118,22 +130,63 @@ public class addVehicle implements Initializable {
             expirydate.setVisible(false);
         }
     }
-    
+
+    public void add() {
+
+        String vregnum = (regnum.getText());
+        String vmodel = (model.getText());
+        String vmake = (make.getText());
+        String vengine = (engine.getText());
+        String vfueltype = (fueltype.getText());
+        String vcolour = (colour.getText());
+        String vmotdate = (motdate.getText());
+        String vlastservice = (lastservice.getText());
+        String vmileage = (mileage.getText());
+
+        String vwarranty = "";
+        if (yes.isSelected()) {
+            vwarranty = yes.getId();
+        } else if (no.isSelected()) {
+            vwarranty = no.getId();
+        }
+
+        String vwarrantycompany = (companyname.getText());
+        String vwarrantyaddress = (companyaddress.getText());
+        String vwarrantyexpiry = (expirydate.getText());
+
+        Database.getInstance().addVehicle(vregnum, vmodel, vmake, vengine, vfueltype, vcolour, vmotdate, vlastservice, vmileage, vwarranty, vwarrantycompany, vwarrantyaddress, vwarrantyexpiry);
+        
+        try
+        {
+            
+            URL addPartUrl = getClass().getResource("/vehicles/gui/VehicleHomepage.fxml");
+            AnchorPane addPartPane = FXMLLoader.load(addPartUrl);
+            
+            BorderPane border = Main.getRoot();
+            
+            border.setCenter(addPartPane);
+        }
+        catch(IOException ex)
+        {
+            ex.printStackTrace();
+        }
+
+    }
+
     @FXML
-    public void clearButton()
-    {
-    regnum.clear();
-    model.clear();
-    make.clear();
-    engine.clear();
-    fueltype.clear();
-    colour.clear();
-    motdate.clear();
-    lastservice.clear();
-    mileage.clear();
-    companyname.clear();
-    companyaddress.clear();
-    expirydate.clear();
+    public void clearButton() {
+        regnum.clear();
+        model.clear();
+        make.clear();
+        engine.clear();
+        fueltype.clear();
+        colour.clear();
+        motdate.clear();
+        lastservice.clear();
+        mileage.clear();
+        companyname.clear();
+        companyaddress.clear();
+        expirydate.clear();
     }
 
 }
